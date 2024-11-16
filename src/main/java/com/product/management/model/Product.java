0057595f2f50a885dd665c5428109de302fc24d4
rajesh <raj.g.son@gmail.com>
2024-11-16 22:57:12 +0530

    Added properties

--- src/main/java/com/product/management/model/Product.java
+++ src/main/java/com/product/management/model/Product.java
@@ -1,10 +1,16 @@
 package com.product.management.model;
 
 import com.product.management.enumuration.Category;
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Data
+@Builder
+@Entity
 public class Product implements Serializable {
 
     private Long id;
